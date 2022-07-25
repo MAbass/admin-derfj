57d1dff59011813bb364d9f34e3927b22d16187e
Cheikh <bayecheikhgueye2@gmail.com>
2022-07-25 15:24:50 +0000

    Update

---
+++ plugins/vjsf.js
@@ -0,0 +1,7 @@
+import VJsf from '@koumoul/vjsf'
+import '@koumoul/vjsf/dist/main.css'
+// load third-party dependencies (markdown-it, vuedraggable)
+// you can also load them separately based on your needs
+import '@koumoul/vjsf/dist/third-party.js'
+
+Vue.component('VJsf', VJsf)
