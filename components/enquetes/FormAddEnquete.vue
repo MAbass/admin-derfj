134d6c7f20c3c4d422e9ca773cd2a8ed7db40a1d
Cheikh <bayecheikhgueye2@gmail.com>
2022-08-20 13:07:31 +0000

    Update

--- components/enquetes/FormAddEnquete.vue
+++ components/enquetes/FormAddEnquete.vue
@@ -508,7 +508,7 @@
         <v-col md="12" lg="12" sm="12">
           <GmapMap
             :center="center"
-            :zoom="15"
+            :zoom="11"
             map-style-id="roadmap"
             :options="mapOptions"
             style="width: 100%; height: 50vmin"
